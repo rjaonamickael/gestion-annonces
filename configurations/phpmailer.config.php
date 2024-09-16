c69b620a4891d53cde98cfd1406237dd34562189
Mickaël <r.jaonamickael@fmail.com>
2024-09-16 19:38:56 -0400

    suppressions de tout

--- configurations/phpmailer.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('HOST', 'smtp.gmail.com');
-define('USERNAME', 'rumourscollective27@gmail.com');
-define('PASSWORD', 'ilqucwuecimisphb');
-define('PORT', '465');
-
-?>
--- configurations/security.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// config.php
-define('HASH_TYPE', 'sha256');
-define('SALT_SIZE', 16);
-
-
-?>
